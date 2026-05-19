b0eb5b78414adf30402092ca79a3e21da122a7a3
sztokmansofia5-ctrl <sztokmansofia5@gmail.com>
2026-05-19 11:31:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+const boton = document.getElementById("btnAccion");
+const mensaje = document.getElementById("mensaje");
 
+boton.addEventListener("click", function () {
+  mensaje.textContent = "¡Gracias por hacer clic! La página está respondiendo a tu acción 😊";
+});
