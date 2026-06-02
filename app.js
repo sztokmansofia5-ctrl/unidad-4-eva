5bb819dbd1adb3dc73bd291e06c023e643bb1bfa
sztokmansofia5-ctrl <sztokmansofia5@gmail.com>
2026-06-02 10:31:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ const mensaje = document.getElementById("mensaje");
 boton.addEventListener("click", function () {
   mensaje.textContent = "¡Gracias por hacer clic! La página está respondiendo a tu acción 😊";
 });
+const campoNombre = document.getElementById("campoNombre");
+const previewNombre = document.getElementById("previewNombre");
+
+campoNombre.addEventListener("input", function () {
+  const valor = campoNombre.value;
+
+  if (valor.trim() === "") {
+    previewNombre.textContent = "Thomasagua";
+  } else {
+    previewNombre.textContent = valor;
+  }
+});
